8ffb419283792a3599845acd7baf3e721c5094df
JoaoPeres <jperesbertoncelo@gmail.com>
2023-04-25 15:12:19 -0300

    Trabalho 4 quase finalizado

--- Trabalho 2 - Técnicas de Aproximação/final.py
+++ Trabalho 2 - Técnicas de Aproximação/final.py
@@ -2,14 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Definindo a função seno
-def f(x):
+def seno_exato(x):
     return np.sin(x)
 
 # Aproximação de Padé de ordem (7,4) elementos
-p = np.poly1d([-121/2268000, 0, 601/118800, 0, -241/1650, 0, 1, 0])
-q = np.poly1d([19/118800, 0, 17/825, 0, 1])
+# Define os coeficientes dos polinômios p(x) e q(x)
+p_coef = [-121/2268000, 0, 601/118800, 0, -241/1650, 0, 1, 0]
+q_coef = [19/118800, 0, 17/825, 0, 1]
+
+# Define as funções para avaliar os polinômios p(x) e q(x) em um dado valor de x
+def eval_poly(p_coef, x):
+    result = 0
+    for i in range(len(p_coef)):
+        result += p_coef[i] * x ** i
+    return result
+
+def eval_p(p_coef, x):
+    return eval_poly(p_coef, x)
+
+def eval_q(q_coef, x):
+    return eval_poly(q_coef, x)
+
+# Define a função pade(x)
 def pade(x):
-    return p(x)/q(x)
+    return eval_p(p_coef, x) / eval_q(q_coef, x)
+
 
 # Aproximação de Taylor com 6 termos
 a = -1/6
@@ -25,10 +42,10 @@ def taylor(x):
 x = np.linspace(-(np.pi/4), (np.pi/4), 10000)
 
 def erro_pade(x):
-    return abs(f(x) - pade(x))
+    return abs(seno_exato(x) - pade(x))
 
 def erro_taylor(x):
-    return abs(f(x) - taylor(x))
+    return abs(seno_exato(x) - taylor(x))
 
 # Cálculo dos valores de y
 #y = f(x)
@@ -45,3 +62,10 @@ plt.plot(x, y_erro_p, label="Erro de Padé")
 plt.plot(x, y_erro_t, label="Erro de Taylor")
 plt.legend()
 plt.show()
+
+#Imprime os resultados
+print(f"O seno de {x} é {seno_exato:.11f}")
+print(f"A aproximação taylor com {n} termos é {taylor:.11f}")
+print(f"A aproximação pade com {n} termos é {pade:.11f}")
+print(f"A diferença taylor é {abs(seno_exato - taylor):.11f}")
+print(f"A diferença pade é {abs(seno_exato - pade):.11f}")
--- Trabalho 4 - In/In.py
+++ Trabalho 4 - In/In.py
@@ -4,42 +4,124 @@
 #y = ln(x/kx) = ln((x/x)*(1/k))
 #y = ln(x/x) + ln(1/k) = 0 + ln(1/k) = -ln(k)
 
+from typing import List, Dict
 import math
+import struct
 
-def ln(x):
-    if x == 1:
-        return 0
-    elif x < 1:
-        k = 1/x
-        return -math.log(k)
-    else:
-        return ln(1/x)
+def ln_x(number: float) -> float:
+    """
+    Retorna o logaritmo natural (ln) de um número usando interpolação a partir de uma tabela predefinida.
+    """
+    # Obtendo a tabela de números Nice
+    nice_table = niceNumbers()
 
-def e(x):
-    return 2.71828182845904523536028747135266249775724709369995**x
+    # Definindo uma variável para percorrer a tabela Nice
+    index = 0
+
+    # Obtendo a linha atual da tabela Nice
+    current_row = nice_table[index]
+
+    # Enquanto o número for maior que o limite inferior da próxima linha da tabela Nice,
+    # avança para a próxima linha
+    while index < len(nice_table) - 1 and nice_table[index + 1]['n'] > number:
+        index += 1
+        current_row = nice_table[index]
+
+    # Dividindo o número pela constante da linha atual da tabela Nice
+    x = number / current_row['n']
+
+    # Obtendo o ln da linha atual da tabela Nice
+    y = current_row['ln']
+
+    # Interpolando o ln do número a partir das próximas linhas da tabela Nice
+    while index < len(nice_table) - 1:
+        # Multiplicando x pela potência da constante da linha atual da tabela Nice
+        power = current_row['exp']
+        mult = x ** power
+
+        # Enquanto o número for maior ou igual ao limite inferior da próxima linha da tabela Nice,
+        # avança para a próxima linha
+        while index < len(nice_table) - 1 and mult >= 1:
+            index += 1
+            current_row = nice_table[index]
+            power = current_row['exp']
+            mult = x ** power
+
+        # Se ainda há linhas para interpolar o ln, atualiza o valor de x e y
+        if index < len(nice_table) - 1:
+            x = mult
+            y -= current_row['ln']
+
+    # Retorna o ln do número subtraído do erro de arredondamento
+    return y - abs(1 - x)
+
 
 #uma tabela de consulta com entradas nice-numbers
-def niceNumbers():
-    return [1.0, 1.125, 1.25, 1.375, 1.5, 1.625, 1.75, 1.875]
+def niceNumbers() -> list[dict]:
+    return [
+    {'n': 3.0, 'ln': 1.0986122886681098, 'exp': 1},
+    {'n': 5.0, 'ln': 1.6094379124341003, 'exp': 2},
+    {'n': 9.0, 'ln': 2.1972245773362196, 'exp': 3},
+    {'n': 17.0, 'ln': 2.833213344056216, 'exp': 4},
+    {'n': 33.0, 'ln': 3.4965075614664802, 'exp': 5},
+    {'n': 65.0, 'ln': 4.174387269895637, 'exp': 6},
+    {'n': 129.0, 'ln': 4.8573324964312685, 'exp': 7},
+    {'n': 257.0, 'ln': 5.543741150080023, 'exp': 8},
+    {'n': 513.0, 'ln': 6.233406007803151, 'exp': 9},
+    {'n': 1025.0, 'ln': 6.926205448990583, 'exp': 10},
+    {'n': 2049.0, 'ln': 7.621945174070975, 'exp': 11},
+    {'n': 4097.0, 'ln': 8.320363047772128, 'exp': 12},
+    {'n': 8193.0, 'ln': 9.021279351957755, 'exp': 13},
+    {'n': 16385.0, 'ln': 9.72452689305461, 'exp': 14},
+    {'n': 32769.0, 'ln': 10.429948609920046, 'exp': 15},
+    {'n': 65537.0, 'ln': 11.137399121336791, 'exp': 16},
+    {'n': 131073.0, 'ln': 11.846740579210332, 'exp': 17},
+    {'n': 262145.0, 'ln': 12.557841045021825, 'exp': 18},
+    {'n': 524289.0, 'ln': 13.270573115025252, 'exp': 19},
+    {'n': 1048577.0, 'ln': 13.984814572476575, 'exp': 20},
+    {'n': 2097153.0, 'ln': 14.700448005475653, 'exp': 21},
+    {'n': 4194305.0, 'ln': 15.417360959112062, 'exp': 22},
+    {'n': 8388609.0, 'ln': 16.135444016864466, 'exp': 23},
+    {'n': 16777217.0, 'ln': 16.85459291287815, 'exp': 24},
+    {'n': 33554433.0, 'ln': 17.57470786432736, 'exp': 25},
+    {'n': 67108865.0, 'ln': 18.295691437997673, 'exp': 26},
+]
 
-#Cálculo e^x e ln(x)
-def calc(x):
-    if x == 0:
-        return 1
-    elif x >= 0:
-        return e(x)
-    else:
-        return ln(x)
+def libc_ln(x: float) -> float:
+    return math.log(x)
+
+def calc(a, e):
+    val = struct.pack('f', a)
+    k = struct.unpack('i', val)[0]
+
+    expoente = (k >> 23) & 0xFF
+
+    aux = a
+
+    expoente += e
+
+    k = (k & ~(0xFF << 23)) | (expoente << 23)
+
+    val = struct.pack('i', k)
+    f = struct.unpack('f', val)[0]
+
+    f += aux
+
+    return f
     
 def main():
-    print("Bem-vindo ao programa de cálculo de e^x e ln(x)!")
-    x = input("Por favor, informe um número: ")
-    try:
-        x = float(x)
-        print(f"e^{x} = {calc(x)}")
-        print(f"ln({x}) = {calc(-x)}")
-    except ValueError:
-        print("O valor informado é inválido. Por favor, informe um número.")
+    numbers = [1, 3, 5, 7, 10, 20, 50, 100, 200, 500, 1000, 5000, 10000]
+    for number in numbers:
+        # Obtém o resultado da nossa função
+        result = ln_x(number)
+        
+        # Compara com o resultado da biblioteca matemática do Python
+        expected = math.log(number)
+        error = abs(expected - result)
+        
+        # Exibe o resultado
+        print(f"ln({number}) = {result:.6f} (erro: {error:.6f})")
+
 
 if __name__ == "__main__":
     main()
